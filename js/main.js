3d48252fbc45912e4cbb0b6950b93f9da648cf5b
stairwaytohell <marzet.rock@gmail.com>
2020-01-09 21:10:09 +0300

    try to add parallax

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const parallax = document.getElementById("parallax");
+window.addEventListener("scroll", function () {
+	let offset = window.pageYOffset;
+	console.log('offset: ' + offset);
+	console.log('offset * 07: ' + offset * 0.7)
+	parallax.style.backgroundPositionY = (offset * 0.7) + "px";
+});
+
 function functionName() {
 	console.log("Hello World");
 }
