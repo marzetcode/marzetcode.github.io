e2431c327055428a3c4d2fabfa52922158ae9ff9
stairwaytohell <marzet.rock@gmail.com>
2020-01-14 01:03:32 +0300

    add telegram link

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ const parallax = document.getElementById("parallax");
 window.addEventListener("scroll", function() {
 	let offset = window.pageYOffset;
 	console.log("offset:" + offset);
+	console.log(parallax.style);
 	parallax.style.backgroundPositionY = offset * 0.8 + "px";
 });
+
+console.log(parallax);
 
 /* function functionName() {
 	console.log("Hello World");
