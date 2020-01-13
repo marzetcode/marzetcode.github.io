4ca43a4751e99ff8073223c01fc56c11743388bf
stairwaytohell <marzet.rock@gmail.com>
2020-01-13 12:38:22 +0300

    parallax

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 const parallax = document.getElementById("parallax");
-window.addEventListener("scroll", function a () {
+window.addEventListener("scroll", function() {
 	let offset = window.pageYOffset;
 	console.log("offset:" + offset);
-	console.log("offset * 08:" + offset * 0.8)
 	parallax.style.backgroundPositionY = offset * 0.8 + "px";
 });
-
-a();
 
 /* function functionName() {
 	console.log("Hello World");
