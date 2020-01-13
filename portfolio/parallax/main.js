a4155b61bdb194e3f18aae1b2368c2f83ab7319a
stairwaytohell <marzet.rock@gmail.com>
2020-01-13 12:56:39 +0300

    add parallax page

--- portfolio/parallax/main.js
+++ portfolio/parallax/main.js
@@ -1,7 +1,6 @@
 const parallax = document.getElementById("parallax");
-window.addEventListener("scroll", function()
-{
+window.addEventListener("scroll", function () {
 	let offset = window.pageYOffset;
 	console.log("offset: " + offset);
-	parallax.style.backgroundPositionY = offset * 0.8 +"px";
+	parallax.style.backgroundPositionY = offset * 0.8 + "px";
 })
