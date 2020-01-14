75d8e33907e4b7ee7b845e1921cfab716796f7cf
stairwaytohell <marzet.rock@gmail.com>
2020-01-14 14:38:14 +0300

    ...

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
+//parallax for header
 const parallax = document.getElementById("parallax");
 console.log(parallax.style);
 document.addEventListener("DOMContentLoaded", function () {
 	window.addEventListener("scroll", function () {
 		let offset = window.pageYOffset;
-		console.log("offset: " + offset);
 		parallax.style.backgroundPositionY = offset * 0.8 + "px";
 	});
 });
--- portfolio/parallax/main.js
+++
@@ -1,6 +0,0 @@
-const parallax = document.getElementById("parallax");
-window.addEventListener("scroll", function () {
-	let offset = window.pageYOffset;
-	console.log("offset: " + offset);
-	parallax.style.backgroundPositionY = offset * 0.8 + "px"
-});
