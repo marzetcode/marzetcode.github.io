650418a63fc9d63f283e95e327156225f86b39e5
stairwaytohell <marzet.rock@gmail.com>
2020-04-06 17:42:44 +0300

    ...

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //parallax for header
 const parallax = document.getElementById('parallax');
-// console.log(parallax.style);
+console.log(parallax.style);
 document.addEventListener('DOMContentLoaded', function () {
 	window.addEventListener('scroll', function () {
 		let offset = window.pageYOffset;
@@ -14,24 +14,24 @@ document.documentElement.style.overflowX = 'hidden';
 
 
 //fade in by scroll
-const blockrowfade = document.getElementsByClassName('blockrow');
+const blockRowFade = document.getElementsByClassName('blockrow');
 let browserH = document.documentElement.clientHeight;
-console.log(blockrowfade[0].style);
+console.log(blockRowFade[0].style);
 console.log('clientHeight: ', browserH);
 document.addEventListener('DOMContentLoaded', function () {
 	window.addEventListener('scroll', function () {
 		let h = 300;
-		let blockrowY = window.pageYOffset;
-		console.log(blockrowY);
+		let blockRowY = window.pageYOffset;
+		console.log(blockRowY);
 		// если высота окна браузера меньше 700 или если страница перезагружена 
 		if ((browserH < 700) || (event.currentTarget.performance.navigation.type == 1)) {
-			for (let i = 0; i < blockrowfade.length; i++) {
-				blockrowfade[i].classList.add('animated', 'fadeIn');
+			for (let i = 0; i < blockRowFade.length; i++) {
+				blockRowFade[i].classList.add('animated', 'fadeIn');
 			};
 		} else
-			for (let i = 0; i < blockrowfade.length; i++) {
-				if ((blockrowY < h) && (blockrowY > h - 50)) {
-					blockrowfade[i].classList.add('animated', 'fadeIn');
+			for (let i = 0; i < blockRowFade.length; i++) {
+				if ((blockRowY < h) && (blockRowY > h - 50)) {
+					blockRowFade[i].classList.add('animated', 'fadeIn');
 				};
 				h = h + 400;
 			};
