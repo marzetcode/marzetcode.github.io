2feaba0d1db7fc0851503d8bd179f16836123846
Marzet <marzet.rock@gmail.com>
2020-08-18 17:21:10 +0300

    ...

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 //parallax for header
-const parallax = document.getElementById('parallax');
-console.log(parallax.style);
+const parallax1 = document.getElementById('parallax');
+console.log(parallax1.style);
 document.addEventListener('DOMContentLoaded', function() {
 	window.addEventListener('scroll', function() {
 		let offset = window.pageYOffset;
-		parallax.style.backgroundPositionY = offset * 0.8 + 'px';
+		parallax1.style.backgroundPositionY = offset * 0.8 + 'px';
 	});
 });
 
