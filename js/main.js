4685783c6e617af05e126017b45f4f9509bc2b58
stairwaytohell <marzet.rock@gmail.com>
2021-06-23 14:03:39 +0300

    ...

--- js/main.js
+++ js/main.js
@@ -11,30 +11,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //hidden horizontal scrollbar because css overflow-x: hidden doesn't work with window.pageYOffset
 document.documentElement.style.overflowX = 'hidden';
-
-
-//fade in by scroll
-//const blockRowFade = document.getElementsByClassName('blockrow');
-//let browserH = document.documentElement.clientHeight;
-//console.log(blockRowFade[0].style);
-//console.log('clientHeight: ', browserH);
-//document.addEventListener('DOMContentLoaded', function() {
-//	window.addEventListener('scroll', function() {
-//		let h = 1000;
-//		let blockRowY = window.pageYOffset;
-//		console.log(blockRowY);
-//		// если высота окна браузера меньше 700 или если страница перезагружена
-//		if ((browserH < 700) || (event.currentTarget.performance.navigation.type == 1)) {
-//			for (let i = 0; i < blockRowFade.length; i++) {
-//				blockRowFade[i].classList.add('animated', 'fadeIn');
-//			};
-//		} else
-//			for (let i = 0; i < blockRowFade.length; i++) {
-//				if ((blockRowY < h) && (blockRowY > h - 100)) {
-//					blockRowFade[i].classList.add('animated', 'fadeIn');
-//				};
-//				h = h + 400;
-//			};
-//	});
-//});
-//fade in by scroll
